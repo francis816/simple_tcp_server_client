4e0b1816cd28795fcba77c85f5df8389ddf970c6
Francis <92971421+francis816@users.noreply.github.com>
2022-05-27 00:04:41 -0500

    ask server simple math questions; change IP

--- client.c
+++ client.c
@@ -3,15 +3,18 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <time.h>
 
 #define SERVER_PORT 9527 
+
+
 void sys_err(const char *string)
 {
 	perror(string);
 	exit(1);
 }
 
-int main()
+int main(int argc, char *argv[])
 {
 	int connectfd;
 
@@ -21,19 +24,30 @@ int main()
 	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(SERVER_PORT);  // match with the server one, little to big endian
-	inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr.s_addr); // convert server ip from string to int, also little to big endian
+	inet_pton(AF_INET, argv[1], &server_address.sin_addr.s_addr); // convert server ip from string to int, also little to big endian
 
 
 	int result = connect(connectfd, (struct sockaddr *)&server_address, sizeof(server_address));
 	if (result != 0) sys_err("connect err"); //sueccess = 0, error = -1
 	
-	printf("hihi");
-	char buffer[BUFSIZ];
-	int i = 10;
-	while(--i)
+	srand((size_t)time(NULL));
+	int num1 = 0;
+	int num2 = 0;
+	char ch = 0;
+	char operator[] = { '+', '-', '*', '/' };
+
+
+	for (int i = 0; i < 20; i++)
 	{
-		write(connectfd, "hello\n", 6); // write to fd
-		result = read(connectfd, buffer, sizeof(buffer)); // read from fd
+		num1 = rand() % 100 + 1;
+		num2 = rand() % 100 + 1;
+		ch = operator[ rand() % 4 ];
+		
+		char buffer[64] = { 0 } ;
+		sprintf(buffer, "Client: What is %d %c %d = ? \n", num1, ch, num2);
+		
+		write(connectfd, buffer, sizeof(buffer)); // write to server
+		result = read(connectfd, buffer, sizeof(buffer)); // read from server
 		write(STDOUT_FILENO, buffer, result); // write to screen
 		sleep(1);
 	}
